ae4643354199426045f64faf927319cedfa036f0
Yanshu Kishore <53146064+superyassh@users.noreply.github.com>
2019-09-23 19:01:39 +0530

    Source Code of LED Flasher

---
+++ Exp1_LED_Flasher/Code File.ino
@@ -0,0 +1,16 @@
+int led_flash=8; //Defines usage of pin 8 for connecting LEDs.
+void setup()
+{
+pinMode(led_flash, OUTPUT);
+// Initialize digital pin led_flash as an output.
+}
+// the loop function runs over and over again forever.
+void loop()
+{
+digitalWrite(led_flash, HIGH);
+// Turn the LED on (HIGH is the voltage level)
+delay(1000); // Making the delay 
+digitalWrite(led_flash, LOW);
+// Turn the LED off by making the voltage LOW
+delay(1000); // Making the delay 
+}
