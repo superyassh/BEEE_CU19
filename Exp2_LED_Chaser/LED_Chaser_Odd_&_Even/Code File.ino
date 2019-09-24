316204575cd0b915bffbf7b765162a9b958ca12c
Yanshu Kishore <53146064+superyassh@users.noreply.github.com>
2019-09-24 23:25:43 +0530

    Source Code / Program of LEDs Chaser ODD & EVEN

--- Exp2_LED_Chaser/LED_Chaser_Odd_&_Even/Code File.ino
+++ Exp2_LED_Chaser/LED_Chaser_Odd_&_Even/Code File.ino
@@ -1,31 +1,31 @@
 //ODD EVEN
 void setup()
 {
-  pinMode(1, OUTPUT);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
+  pinMode(1, OUTPUT); // Initialize digital pin led_flash as an output.
+  pinMode(2, OUTPUT); // Initialize digital pin led_flash as an output.
+  pinMode(3, OUTPUT); // Initialize digital pin led_flash as an output.
+  pinMode(4, OUTPUT); // Initialize digital pin led_flash as an output.
   
 }
-void loop()
+void loop() // the loop function runs over and over again forever.
 { 
   for(int i=0;i<4;i++)
   {
     if(i==0)
     {
-     digitalWrite(2, HIGH);
-     digitalWrite(4, HIGH);
-  	 delay(1000);
-  	 digitalWrite(2, LOW);
-     digitalWrite(4, LOW); 
+     digitalWrite(2, HIGH); // Turn the LED on (HIGH is the voltage level)
+     digitalWrite(4, HIGH); // Turn the LED on (HIGH is the voltage level)
+  	 delay(1000); // Making the delay 
+  	 digitalWrite(2, LOW); // Turn the LED OFF (OFF is the voltage level)
+     digitalWrite(4, LOW); // Turn the LED OFF (OFF is the voltage level)
     }
     if(i==1)
     {
-     digitalWrite(1, HIGH);
-     digitalWrite(3, HIGH);
-     delay(1000);
-     digitalWrite(1, LOW);
-     digitalWrite(3, LOW); 
+     digitalWrite(1, HIGH); // Turn the LED on (HIGH is the voltage level)
+     digitalWrite(3, HIGH); // Turn the LED on (HIGH is the voltage level)
+     delay(1000); // Making the delay
+     digitalWrite(1, LOW); // Turn the LED OFF (OFF is the voltage level)
+     digitalWrite(3, LOW); // Turn the LED OFF (OFF is the voltage level)
     }
   }
 }
