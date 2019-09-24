a0e411f12c44bf9233b1105309b66b048f40a98a
Yanshu Kishore <53146064+superyassh@users.noreply.github.com>
2019-09-24 21:24:09 +0530

    Source code of LED Chaser with Delay

---
+++ Exp2_LED_Chaser/LED_Chaser_with_Delay/Code File.ino
@@ -0,0 +1,20 @@
+//LED CHASER with Delay
+void setup()
+{
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+ // Initialize digital pin led_flash as an output.
+}
+void loop()
+// the loop function runs over and over again forever.
+{ 
+  for(int i=1;i<=4;i++)
+  {
+     digitalWrite(i,HIGH); // Turn the LED on (HIGH is the voltage level)
+  	 delay(500); // Making the delay 
+  	 digitalWrite(i,LOW); // Turn the LED OFF (OFF is the voltage level)
+     delay(500); // Making the delay 
+  }
+}
